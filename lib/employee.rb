1224279754dd677c80dd927a700a603c46b0a0e1
keepitclassick <keepitclassick@gmail.com>
2022-01-03 16:11:51 -0500

    add validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+    # first/last name must be present
+    validates :first_name, :last_name, presence: true
+
+    # hourly rate must be integer 40-200
+    validates :hourly_rate, numericality: {only_integer: true, greater_than: 40, less_than: 200}
+  
+    # employee must belong to a store
+    validates :store_id, presence: true
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  # Name must be minimum 3 characters
+  validates :name, length: { minimum: 3 }
+
+  # Revenue must be integer >= 0
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: -1}
 end
