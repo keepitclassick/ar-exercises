2d36f064d3b4158738ca1a43bac47628de3ebd7b
keepitclassick <keepitclassick@gmail.com>
2022-01-03 16:09:59 -0500

    create one to many rel and complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Jessica", last_name: "Lut", hourly_rate: 34)
+
+@store1.employees.create(first_name: "Sammy", last_name: "Sosa", hourly_rate: 53)
+
+@store2.employees.create(first_name: "Conor", last_name: "Johnston", hourly_rate: 100)
+
+@store2.employees.create(first_name: "Jack", last_name: "Goddard", hourly_rate: 22)
+
+@store2.employees.create(first_name: "Dax", last_name: "Berg", hourly_rate: 33)
