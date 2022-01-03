19994ee4ea3b73473063148f0d095553058257a2
keepitclassick <keepitclassick@gmail.com>
2022-01-03 16:16:36 -0500

    complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -6,4 +6,12 @@ class Store < ActiveRecord::Base
 
   # Revenue must be integer >= 0
   validates :annual_revenue, numericality: { only_integer: true, greater_than: -1}
+  # Custom validation, must at least carry either men's or women's
+  validates :must_carry_mens_or_womens
+
+    def must_carry_mens_or_womens 
+      if (!mens_apparel && !womens_apparel)
+        errors.add(:store, "must carry at least one of men's or women's apparel")
+      end
+    end
 end
